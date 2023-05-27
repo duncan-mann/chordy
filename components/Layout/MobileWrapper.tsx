4864c9901be94805a6a856176dff025b173e9faf
Duncan <duncan.s.mann@gmail.com>
2023-05-27 10:27:49 -0400

    feat: make mobile wrapper prettier

--- components/Layout/MobileWrapper.tsx
+++ components/Layout/MobileWrapper.tsx
@@ -5,13 +5,15 @@ export const MobileWrapper = ({ children }: PropsWithChildren): JSX.Element => {
   const { width } = useWindowDimensions()
   return width < 400 ? (
     <div
-      className={`w-full flex justify-center items-center min-h-screen bg-[url('./images/CAGED-Background.jpeg')] bg-no-repeat bg-center bg-cover`}
+      className={`w-full flex flex-col min-h-screen bg-[url('./images/CAGED-Background.jpeg')] bg-no-repeat bg-center bg-cover pt-32`}
     >
-      <h1 className="text-white font-bold font-poppins text-lg text-center p-3">
-        {
-          "We're currently working on mobile optimization, check us out on desktop!"
-        }
-      </h1>
+      <h1 className="text-center text-6xl mb-4">{'🎸'}</h1>
+      <h2 className="text-white font-bold font-poppins text-2xl text-center px-4 mb-7 ">
+        {'Mobile is currently in development!'}
+      </h2>
+      <p className="text-white font-bold font-poppins text-sm text-center">
+        {'Come check us out on desktop'}
+      </p>
     </div>
   ) : (
     <>{children}</>
